47740a1a5e99d5d722d304bb38f99b91e9080242
mateu52 <wuoelte@gmail.com>
2021-11-25 05:25:54 +0100

    zad2 loading

--- src/components/zad2/List.js
+++ src/components/zad2/List.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Routes, Route, Link } from "react-router-dom";
+import UserDetail from '../UserDetail';
 function List({userData}){
     const checkVal = (value, text) => {
         if (!value) {
@@ -8,27 +10,37 @@ function List({userData}){
     }
     return(
         <div className="userCss">
+            <Router>
              {userData.map((user) => {
                 return (
-                    <div key={user.login.uuid} className="userMap">
-                        <div className="inMap">    
-                            <p><img alt="foto" src={user.picture.large}></img></p>
-                            <p>
-                                {checkVal(user.name.first, "Nie podano imienia")} 
-                                {checkVal(user.name.last, "Nie podano nazwiska")}
-                            </p>
-                            <p>
-                                {checkVal(user.location.street.name, "brak")}: 
-                                {checkVal(user.location.street.number, "brak")}
-                            </p>
-                            <p>{user.location.city}</p>
-                            <p>{user.email}</p>
-                            <p>{new Date(user.registered.date).toDateString()}</p>
-                            
+                   
+                        <div key={user.login.uuid} className="userMap">
+                                <div className="inMap">    
+                                    <p><img alt="foto" src={user.picture.large}></img></p>
+                                    <p>
+                                    <Link to="/UserDetail">
+                                        {checkVal(user.name.first, "Nie podano imienia")} 
+                                        {checkVal(user.name.last, "Nie podano nazwiska")}
+                                    </Link>
+                                    </p>
+                                    <p>
+                                        {checkVal(user.location.street.name, "brak")}: 
+                                        {checkVal(user.location.street.number, "brak")}
+                                    </p>
+                                    <p>{user.location.city}</p>
+                                    <p>{user.email}</p>
+                                    <p>{new Date(user.registered.date).toDateString()}</p>
+                                </div>
+                                
                         </div>
-                    </div>
+                                    
+                   
                 )
             })} 
+                <Routes> // do naprawy
+                    <Route path="UserDetail" element={<UserDetail/>}/>
+                </Routes>
+            </Router>
         </div>
     )
     
---
+++ src/components/zad2/UserDetail.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import UserList from './UserList';
+import { BrowserRouter as Router, Switch, Routes, Route, Link } from "react-router-dom";
+
+function UserDetail(){
+
+    return(
+        
+            <div>
+                <h1>User list</h1>
+                
+                
+            </div>
+        
+        
+    );
+}
+export default UserDetail;
