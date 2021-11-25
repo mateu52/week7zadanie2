0912668cb3e9cc11162267a28a7d1dab8248933f
mateu52 <wuoelte@gmail.com>
2021-11-25 22:15:37 +0100

    zad2 onClicl handle each

--- src/components/zad2/App2.js
+++ src/components/zad2/App2.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Home from './Home';
 import List from './List';
 import { BrowserRouter as Router, Switch, Routes, Route, Link } from "react-router-dom";
-
+import UserDetail from '../UserDetail';
 function App2(){
 
     return(
--- src/components/zad2/List.js
+++ src/components/zad2/List.js
@@ -1,27 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { BrowserRouter as Router, Switch, Routes, Route, Link } from "react-router-dom";
 import UserDetail from '../UserDetail';
+import Home from './Home';
 function List({userData}){
+    const [idUser,setIdUser] = useState(null);
     const checkVal = (value, text) => {
         if (!value) {
             return text;
         }
         return value;
     }
+    const handleId=(event) => {
+        setIdUser(event);
+    }
     return(
         <div className="userCss">
-            <Router>
              {userData.map((user) => {
                 return (
-                   
+                    <div>
                         <div key={user.login.uuid} className="userMap">
                                 <div className="inMap">    
-                                    <p><img alt="foto" src={user.picture.large}></img></p>
+                                    <p><img alt="foto" src={user.picture.large}
+                                        onClick={() => {handleId(user.login.uuid)}}
+                                    ></img></p>
                                     <p>
-                                    <Link to="/UserDetail">
                                         {checkVal(user.name.first, "Nie podano imienia")} 
                                         {checkVal(user.name.last, "Nie podano nazwiska")}
-                                    </Link>
+                                    
                                     </p>
                                     <p>
                                         {checkVal(user.location.street.name, "brak")}: 
@@ -31,16 +36,16 @@ function List({userData}){
                                     <p>{user.email}</p>
                                     <p>{new Date(user.registered.date).toDateString()}</p>
                                 </div>
-                                
                         </div>
+                    
+                    
+                    </div>
+
                                     
                    
                 )
-            })} 
-                <Routes> // do naprawy
-                    <Route path="UserDetail" element={<UserDetail/>}/>
-                </Routes>
-            </Router>
+            })}
+
         </div>
     )
     
